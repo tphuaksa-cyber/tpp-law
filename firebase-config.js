9199483a92c44ceca415650e26652033b5a043e8
tphuaksa-cyber <tphuaksa@gmail.com>
2026-05-08 15:15:40 +0700

    Refactor: Move Firebase configuration to firebase-config.js to resolve security warning

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDNfkNuiHP6IWnBWhjgrO5Cr1ogqr5vORc",
+    authDomain: "tpp-law.firebaseapp.com",
+    databaseURL: "https://tpp-law-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "tpp-law",
+    storageBucket: "tpp-law.firebasestorage.app",
+    messagingSenderId: "964367025099",
+    appId: "1:964367025099:web:01dccb5abbd48b0fec2c4e"
+};
